c420a13bb093fe688c48379d34acb2ba02b70262
owner <owner@L13-IT-HAINIZAM.local>
2024-04-27 13:04:35 +0800

    initial commit

---
+++ swift_test_2704/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swift_test_2704
+//
+//  Created by owner on 27/04/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
